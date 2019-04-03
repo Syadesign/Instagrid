7f3890face935a120aa8c9fd071e5ad3730244d2
Samahir <samahiradi@gmail.com>
2019-04-03 12:20:46 +0200

    Create instagrid project, create the views and a modelView class

---
+++ Instagrid/View/ModelView.swift
@@ -0,0 +1,68 @@
+//
+//  ModelView.swift
+//  Instagrid
+//
+//  Created by samahir adi on 03/04/2019.
+//  Copyright © 2019 Syadesign. All rights reserved.
+//
+
+import UIKit
+
+class ModelView: UIView {
+    
+    @IBOutlet var model1 :ModelView?
+    @IBOutlet var model2 :ModelView?
+    @IBOutlet var model3 :ModelView?
+    @IBOutlet var selected1 :UIImageView?
+    @IBOutlet var selected2 :UIImageView?
+    @IBOutlet var selected3 :UIImageView?
+    
+    @IBAction func choice1(_ sender: Any) {
+        setModel(.model1)
+    }
+    
+    @IBAction func choice2(_ sender: Any) {
+        setModel(.model2)
+    }
+    
+    @IBAction func choice3(_ sender: Any) {
+        setModel(.model3)
+    }
+    
+    enum Model  {
+        case model1, model2, model3
+    }
+    
+    var model: Model = .model2 {
+        didSet {
+            setModel (model)
+        }
+    }
+
+    func setModel (_ model: Model) {
+        switch model {
+        case .model1:
+            model1?.isHidden = false
+            model2?.isHidden = true
+            model3?.isHidden = true
+            selected1?.isHidden = false
+            selected2?.isHidden = true
+            selected3?.isHidden = true
+        case .model2:
+            model1?.isHidden = true
+            model2?.isHidden = false
+            model3?.isHidden = true
+            selected1?.isHidden = true
+            selected2?.isHidden = false
+            selected3?.isHidden = true
+        case .model3:
+            model1?.isHidden = true
+            model2?.isHidden = true
+            model3?.isHidden = false
+            selected1?.isHidden = true
+            selected2?.isHidden = true
+            selected3?.isHidden = false
+        }
+        }
+    }
+
