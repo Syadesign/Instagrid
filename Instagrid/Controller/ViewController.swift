03dc9f3123b99c4f56c513cce0ab817cef8a8d2b
Samahir <samahiradi@gmail.com>
2019-04-09 16:54:43 +0200

    Add some constraints, create a GridView class, begin the connection between the view and the controller

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -15,10 +15,57 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var logo: UIImageView!
 
+    @IBOutlet weak var swipeIcon: UIImageView!
+    
+    @IBOutlet weak var swipeLabel: UILabel!
+    
+    @IBOutlet weak var gridView: GridView!
+    
+    @IBOutlet weak var bottomRight: UIButton!
+    @IBOutlet weak var bottomLeft: UIButton!
+    @IBOutlet weak var topRight: UIButton!
+    @IBOutlet weak var topLeft: UIButton!
     
     
+    @IBAction func topLeftButton(_ sender: Any) {
+    }
+    
+    @IBAction func topRightButton(_ sender: Any) {
+    }
+    
+    @IBAction func bottomLeftButton(_ sender: Any) {
+    }
+    
+    @IBAction func bottomRightButton(_ sender: Any) {
+    }
+    
+    enum Style {
+        case model1, model2, model3
+    }
+    
+    private func setStyle(_ style: Style) {
+        switch style {
+        case .model1:
+            topLeft.isHidden = true
+        case .model2:
+            bottomLeft.isHidden = true
+        case .model3:
+            topLeft.isHidden = false
+        }
+    }
 
+    @IBAction func buttonModel1(_ sender: Any) {
+        setStyle(.model1)
+    }
     
+    @IBAction func buttonModel2(_ sender: Any) {
+        setStyle(.model2)
+    }
+    
+    @IBAction func buttonModel3(_ sender: Any) {
+        setStyle(.model3)
+    }
 }
 
---
+++ Instagrid/View/GridView.swift
@@ -0,0 +1,19 @@
+//
+//  GridView.swift
+//  Instagrid
+//
+//  Created by samahir adi on 09/04/2019.
+//  Copyright © 2019 Syadesign. All rights reserved.
+//
+
+import UIKit
+
+class GridView: UIView {
+    @IBOutlet var gridStackView :UIStackView?
+    @IBOutlet var topStackView :UIStackView?
+    @IBOutlet var bottomStackView :UIStackView?
+    
+  
+
+ 
+}
